db7b6c13ceb0e18d6316c6a680179ab3c231211b
Vibha Saroj <vibhasaroj14@gmail.com>
2023-02-02 15:26:35 +0530

    Third commit

--- bubbleSort.java
+++ bubbleSort.java
@@ -19,10 +19,25 @@ public class bubbleSort{
         }
 
     }
+
+    static void selectionSort(int[] arr){
+        int n =arr.length;
+        for(int j=i;j<n;j++){
+            int key= arr[j];
+            int i =j-1;
+            while((i>-1)&&(arr[i]>k)){
+                arr[i+1] =arr[i];
+                i--;
+            }
+            arr[i+1]=key;
+        }
+
+    }
     
 
     public static void main(String[] args){
         int arr={10,45,78,31,5};
         bubbleSort(arr);
         insertionSort(arr);
+        selectionSort(arr);
     }
